47cf8dfba04300a554942991db61c1d4eec4f411
Deekshith-Madhunala <madhunaladeekshith@gmail.com>
2024-12-19 23:33:12 -0600

    setup and CRUD operations

---
+++ Movie.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+class Movie(BaseModel):
+    id: int = Field()
+    title: str = Field(min_length=1)
+    description: str = Field(min_length=10)
+    release_year: int = Field(gt=1900)
+    rating: int = Field(gt=3, lt=10)
+
+Movies = []
+
+@app.get('/')
+def greeting():
+    return Movies
+
+@app.post('/')
+def create_movie(movie: Movie):
+    Movies.append(movie)
+    return movie
+
+@app.delete('/{movie_id}')
+def delete_movie(movie_id: int):
+    for index, x in enumerate(Movies):
+        if x.id == movie_id:
+            del Movies[index]
+            return {"detail": "Movie deleted successfully"}
+    raise HTTPException(
+        status_code=404, detail=f"Movie with id: {movie_id} not found"
+    )
+
+@app.put('/{movie_id}')
+def update_movie(movie_id: int, movie: Movie):
+    for index, x in enumerate(Movies):
+        print(f"Checking movie with id {x.id} against {movie_id}")
+        if x.id == movie_id:
+            Movies[index] = movie
+            return Movies[index]
+    raise HTTPException(
+        status_code=404, detail=f"Movie with id: {movie_id} not found"
+    )
